499051e68966d8e7daeff93ba3505e8341e86015
Drevets <drevets@gmail.com>
2018-06-16 20:45:30 -0500

    circle is jumping to finger

--- App.js
+++ App.js
@@ -5,9 +5,10 @@ import {SoundBoardFast} from './app/main'
 import {SoundBoardFaster} from './app/main'
 import {MakeASimpleSound} from './app/sound'
 import {PanResponderExample2} from './app/touches'
+import {PanResponderExample3} from './app/touches'
 
 export default class App extends React.Component {
   render() {
-    return <PanResponderExample2/>;
+    return <PanResponderExample3/>;
   }
 }
